bc9dfd6a1624bf25928fe7a12828de292e45ae40
zack-klein <klein.zachary.j@gmail.com>
2020-08-23 09:48:00 -0700

    feat: Remove git tagging

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,13 +6,6 @@ echo "Setting up aws cli and node..."
 pip install awscli
 sudo apt-get update && sudo apt-get install nodejs -y
 
-
-echo "Tagging repo..."
-pip install chronos-cli
-GIT_TAG=$(chronos infer)
-git tag $GIT_TAG -a -m "[ci skip] Generated tag $GIT_TAG for build $TRAVIS_BUILD_NUMBER"
-git push https://${GIT_TOKEN}@github.com/${GIT_ORG}/${GIT_REPO}.git --tags
-
 echo "Installing Terraform..."
 cd ./terraform/
 wget https://releases.hashicorp.com/terraform/0.12.8/terraform_0.12.8_linux_amd64.zip

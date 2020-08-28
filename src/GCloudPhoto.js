dac310d42d3887739d29634577b881d9083b2a16
zack-klein <klein.zachary.j@gmail.com>
2020-08-28 15:15:43 -0700

    feat: Update powered by image

--- src/GCloudPhoto.js
+++ src/GCloudPhoto.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import CardMedia from '@material-ui/core/CardMedia';
-import gcloudImg from './assets/images/power-by-cloud.png';
 
 
 export default function GCloudPhoto() {
   return (
     <React.Fragment>
+    <br /><br />
     <CardMedia>
-      <img src={gcloudImg} width="100%" alt="recipe thumbnail"/>
+      <img src="https://d1.awsstatic.com/logos/aws-logo-lockups/poweredbyaws/PB_AWS_logo_RGB_stacked.547f032d90171cdea4dd90c258f47373c5573db5.png" alt="Powered by AWS Cloud Computing"></img>
     </CardMedia>
     </React.Fragment>
   );

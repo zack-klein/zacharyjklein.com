b55584db91e4f1f8dd131affef7511cd1e43f552
zack-klein <klein.zachary.j@gmail.com>
2020-09-07 15:23:35 -0400

    fix: Terraform version in CI

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,10 +8,10 @@ sudo apt-get update && sudo apt-get install nodejs -y
 
 echo "Installing Terraform..."
 cd ./terraform/
-wget https://releases.hashicorp.com/terraform/0.12.8/terraform_0.12.8_linux_amd64.zip
-unzip terraform_0.12.8_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.12.29/terraform_0.12.29_linux_amd64.zip
+unzip terraform_0.12.29_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
-rm terraform_0.12.8_linux_amd64.zip
+rm terraform_0.12.29_linux_amd64.zip
 
 echo "Building infrastructure..."
 terraform init

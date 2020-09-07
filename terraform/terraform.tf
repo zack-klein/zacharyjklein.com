0e086edab0d2be35f8323dd6bf686228c5e8b22e
zack-klein <klein.zachary.j@gmail.com>
2020-09-07 15:20:34 -0400

    feat: Add route53 entry

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -22,3 +22,22 @@ module "website" {
   bucket_name = "zacharyjklein.com"
   acm_arn = data.aws_acm_certificate.web_cert.arn
 }
+
+data "aws_route53_zone" "zone" {
+  name         = "zacharyjklein.com."
+  private_zone = false
+}
+
+# Route 53
+
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "zacharyjklein.com"
+  type    = "A"
+
+  alias {
+    name                   = module.website.cloudfront_distribution.domain_name
+    evaluate_target_health = true
+    zone_id                = module.website.cloudfront_distribution.hosted_zone_id
+  }
+}

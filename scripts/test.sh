5e0e58c8c06c7620a3ec2831880493e6d45306bb
zack-klein <klein.zachary.j@gmail.com>
2020-09-24 14:12:51 -0400

    fix: Terraform CLI version in test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # TODO: Add tests!
-cd ./terraform
-
-wget https://releases.hashicorp.com/terraform/0.12.8/terraform_0.12.8_linux_amd64.zip
-unzip terraform_0.12.8_linux_amd64.zip
+cd ./terraform/
+wget https://releases.hashicorp.com/terraform/0.12.29/terraform_0.12.29_linux_amd64.zip
+unzip terraform_0.12.29_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
-rm terraform_0.12.8_linux_amd64.zip
+rm terraform_0.12.29_linux_amd64.zip
 
 terraform init
 terraform plan

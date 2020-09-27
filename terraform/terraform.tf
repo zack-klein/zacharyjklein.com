5fe8b1758b6562e3c44c78c805fdb2048771d2fd
zack-klein <klein.zachary.j@gmail.com>
2020-09-27 09:23:21 -0400

    feat: Add SES identity

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -18,9 +18,9 @@ data "aws_acm_certificate" "web_cert" {
 }
 
 module "website" {
-  source = "github.com/zack-klein/s3-website"
+  source      = "github.com/zack-klein/s3-website"
   bucket_name = "zacharyjklein.com"
-  acm_arn = data.aws_acm_certificate.web_cert.arn
+  acm_arn     = data.aws_acm_certificate.web_cert.arn
 }
 
 data "aws_route53_zone" "zone" {
@@ -40,4 +40,17 @@ resource "aws_route53_record" "www" {
     evaluate_target_health = true
     zone_id                = module.website.cloudfront_distribution.hosted_zone_id
   }
+}
+
+# SES
+resource "aws_ses_domain_identity" "email" {
+  domain = "zacharyjklein.com"
+}
+
+resource "aws_route53_record" "example_amazonses_verification_record" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "_amazonses.example.com"
+  type    = "TXT"
+  ttl     = "600"
+  records = [aws_ses_domain_identity.email.verification_token]
 }

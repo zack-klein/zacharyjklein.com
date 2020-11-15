7e937763414eb2748c475bf0be54e67cbb28fe05
zack-klein <klein.zachary.j@gmail.com>
2020-11-14 19:55:37 -0500

    fix: node version

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,6 +5,7 @@ set -e
 echo "Setting up aws cli and yarn..."
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt update && sudo apt install yarn nodejs -y
 
 echo "Installing Terraform..."

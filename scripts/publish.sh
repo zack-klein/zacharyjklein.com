1bb597170c59e38a8a0cf290cc575c20d20b56d1
zack-klein <klein.zachary.j@gmail.com>
2020-11-14 19:50:23 -0500

    fix: Add node to install script in CI

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,7 +5,7 @@ set -e
 echo "Setting up aws cli and yarn..."
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update && sudo apt install yarn -y
+sudo apt update && sudo apt install yarn node -y
 
 echo "Installing Terraform..."
 cd ./terraform/
